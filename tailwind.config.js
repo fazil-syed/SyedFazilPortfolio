470abbcb32ae0d8723c8dd3af9fdb9c1bbadc13c
fazil-syed <syedfazil539@gmail.com>
2024-04-10 17:49:49 +0530

    herosection

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,237 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}",
+    "./src/**/*.{js,jsx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    fontSize: {
+      "heading1-bold": [
+        "6rem",
+        {
+          lineHeight: "140%",
+          fontWeight: "800",
+        },
+      ],
+      "heading1-semibold": [
+        "36px",
+        {
+          lineHeight: "140%",
+          fontWeight: "800",
+        },
+      ],
+      "heading2-bold": [
+        "3rem",
+        {
+          lineHeight: "140%",
+          fontWeight: "800",
+        },
+      ],
+      "heading2-semibold": [
+        "30px",
+        {
+          lineHeight: "140%",
+          fontWeight: "600",
+        },
+      ],
+      "heading3-bold": [
+        "24px",
+        {
+          lineHeight: "140%",
+          fontWeight: "700",
+        },
+      ],
+      "heading4-medium": [
+        "20px",
+        {
+          lineHeight: "140%",
+          fontWeight: "500",
+        },
+      ],
+      "body-bold": [
+        "1.2rem",
+        {
+          lineHeight: "140%",
+          fontWeight: "700",
+        },
+      ],
+      "body-semibold": [
+        "18px",
+        {
+          lineHeight: "140%",
+          fontWeight: "600",
+        },
+      ],
+      "body-medium": [
+        "18px",
+        {
+          lineHeight: "140%",
+          fontWeight: "500",
+        },
+      ],
+      "body-normal": [
+        "18px",
+        {
+          lineHeight: "140%",
+          fontWeight: "400",
+        },
+      ],
+      "body1-bold": [
+        "18px",
+        {
+          lineHeight: "140%",
+          fontWeight: "700",
+        },
+      ],
+      "base-regular": [
+        "16px",
+        {
+          lineHeight: "140%",
+          fontWeight: "400",
+        },
+      ],
+      "base-medium": [
+        "16px",
+        {
+          lineHeight: "140%",
+          fontWeight: "500",
+        },
+      ],
+      "base-semibold": [
+        "16px",
+        {
+          lineHeight: "140%",
+          fontWeight: "600",
+        },
+      ],
+      "base1-semibold": [
+        "16px",
+        {
+          lineHeight: "140%",
+          fontWeight: "600",
+        },
+      ],
+      "small-regular": [
+        "14px",
+        {
+          lineHeight: "140%",
+          fontWeight: "400",
+        },
+      ],
+      "small-medium": [
+        "14px",
+        {
+          lineHeight: "140%",
+          fontWeight: "500",
+        },
+      ],
+      "small-semibold": [
+        "14px",
+        {
+          lineHeight: "140%",
+          fontWeight: "600",
+        },
+      ],
+      "subtle-medium": [
+        "12px",
+        {
+          lineHeight: "16px",
+          fontWeight: "500",
+        },
+      ],
+      "subtle-semibold": [
+        "12px",
+        {
+          lineHeight: "16px",
+          fontWeight: "600",
+        },
+      ],
+      "tiny-medium": [
+        "10px",
+        {
+          lineHeight: "140%",
+          fontWeight: "500",
+        },
+      ],
+      "x-small-semibold": [
+        "7px",
+        {
+          lineHeight: "9.318px",
+          fontWeight: "600",
+        },
+      ],
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "primary-500": "#877EFF",
+        "secondary-500": "#FFB620",
+        blue: "#0095F6",
+        "logout-btn": "#FF5A5A",
+        "navbar-menu": "rgba(16, 16, 18, 0.6)",
+        "dark-1": "#000000",
+        "dark-2": "#121417",
+        "dark-3": "#101012",
+        "dark-4": "#1F1F22",
+        "light-1": "#FFFFFF",
+        "light-2": "#EFEFEE",
+        "light-3": "#7878A3",
+        "light-4": "#5C5C7B",
+        "gray-1": "#697C89",
+        glassmorphism: "rgba(16, 16, 18, 0.60)",
+      },
+      boxShadow: {
+        "count-badge": "0px 0px 6px 2px rgba(219, 188, 159, 0.30)",
+        "groups-sidebar": "-30px 0px 60px 0px rgba(28, 28, 31, 0.50)",
+      },
+      screens: {
+        xs: "400px",
+      },
+    },
+  },
+  animation: {
+    "gradient-x": "gradient-x 15s ease infinite",
+    "gradient-y": "gradient-y 15s ease infinite",
+    "gradient-xy": "gradient-xy 15s ease infinite",
+  },
+  keyframes: {
+    "gradient-y": {
+      "0%, 100%": {
+        "background-size": "400% 400%",
+        "background-position": "center top",
+      },
+      "50%": {
+        "background-size": "200% 200%",
+        "background-position": "center center",
+      },
+    },
+    "gradient-x": {
+      "0%, 100%": {
+        "background-size": "200% 200%",
+        "background-position": "left center",
+      },
+      "50%": {
+        "background-size": "200% 200%",
+        "background-position": "right center",
+      },
+    },
+    "gradient-xy": {
+      "0%, 100%": {
+        "background-size": "400% 400%",
+        "background-position": "left center",
+      },
+      "50%": {
+        "background-size": "200% 200%",
+        "background-position": "right center",
       },
     },
   },
